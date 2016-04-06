6ff5d2bd4fc972115c4a3a966d2d97e956d9d51a
Liang <liang.zhang.cs@gmail.com>
2016-04-05 21:35:12 -0700

    refactor JCE to a separate method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,11 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         listProviders();
-        Provider jce = Security.getProvider("SunJCE");
-        Set<Provider.Service> services =  jce.getServices();
-        for(Provider.Service service : services) {
-            System.out.println(join(":::", service.getAlgorithm(), service.getType()));
-        }
+
     }
 
     private static void listProviders() {
@@ -23,6 +19,14 @@ public class Main {
         for (Provider provider : providers) {
             System.out.println(join(":::", provider.getName(), String.valueOf(provider.getVersion()), provider.getInfo()));
             provider.getServices();
+        }
+    }
+
+    private static void listServiceFromJCE() {
+        Provider jce = Security.getProvider("SunJCE");
+        Set<Provider.Service> services =  jce.getServices();
+        for(Provider.Service service : services) {
+            System.out.println(join(":::", service.getAlgorithm(), service.getType()));
         }
     }
 }
